989e4523aacc62ba7d90a2f8b0cb463da7094bd9
mfmatt <matt@fusiononline.com>
2021-01-29 17:27:17 -0600

    Modular templates and styling for landing pages

--- js/backdrop.js
+++ js/backdrop.js
@@ -1,38 +1,62 @@
 (function () {
-  if (document.querySelector('#connect-with-your-customers-vendors-and-prospects')) {
-    setTimeout(function () {
-      var container = document.querySelector('#connect-with-your-customers-vendors-and-prospects');
-      var scene = new THREE.Scene();
-      var camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-      var renderer = new THREE.WebGLRenderer({ alpha: true });
-      renderer.setSize(container.offsetHeight, container.offsetHeight);
-      container.appendChild(renderer.domElement);
+  const cubeAnimation = (element) => {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer({ alpha: true });
 
-      var geometry = new THREE.BoxGeometry();
-      var material = new THREE.MeshBasicMaterial({
-        color: 0xa3cfbb,
-        wireframe: true
-      });
-      var cube = new THREE.Mesh(geometry, material);
-      scene.add(cube);
+    renderer.setSize(element.offsetHeight, element.offsetHeight);
+    element.appendChild(renderer.domElement);
 
-      camera.position.z = 1.5;
+    var geometry = new THREE.BoxGeometry();
 
-      var animate = function () {
-        requestAnimationFrame(animate);
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+    var material = new THREE.MeshBasicMaterial({
+      color: 0xa3cfbb,
+      wireframe: true
+    });
 
-        renderer.render(scene, camera);
-      };
+    var cube = new THREE.Mesh(geometry, material);
 
-      animate();
+    scene.add(cube);
+    camera.position.z = 1.5;
 
-      window.addEventListener('resize', onWindowResize);
+    var animate = function () {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.001;
+      cube.rotation.y += 0.001;
+      renderer.render(scene, camera);
+    };
 
-      function onWindowResize() {
-        renderer.setSize(container.offsetHeight, container.offsetHeight);
+    animate();
+
+    window.addEventListener('resize', (event) => { 
+      renderer.setSize(element.offsetHeight, element.offsetHeight);
+    });
+  };
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const lazyLoadVideos = document.querySelectorAll('video[data-autoplay]');
+
+    if (lazyLoadVideos.length > 0) {
+      lazyLoadVideos.forEach((video) => video.addEventListener('canplay', (event) => video.play()));
+
+      if ('IntersectionObserver' in window) {
+        const videoObserver = new IntersectionObserver((videos) => {
+          videos.forEach((video) => {
+            if (video.intersectionRatio <= 0) return;
+
+            videoObserver.unobserve(video.target);
+            video.target.load();
+          });
+        });
+
+        lazyLoadVideos.forEach((video) => videoObserver.observe(video));
+      } else {
+        lazyLoadVideos.forEach((video) => video.load());
       }
-    }, 500);
-  }
+    }
+
+    if (document.querySelector('#connect-with-your-customers-vendors-and-prospects')) {
+      cubeAnimation(document.querySelector('#connect-with-your-customers-vendors-and-prospects'));
+    }
+  });
 })();
