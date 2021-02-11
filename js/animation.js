fe27a9064edcb6b8d9f5292432eabaf16c1abbab
mfmatt <matt@fusiononline.com>
2021-02-11 17:07:56 -0600

    Home page

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,4 @@
-(function () {
+(function (anime) {
   const cubeAnimation = (element) => {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
@@ -27,9 +27,91 @@
     });
   };
 
+  const movingLettersEffect12 = (element) => {
+
+    anime({
+      targets: element.querySelectorAll('.letter'),
+      translateX: [40, 0],
+      translateZ: 0,
+      opacity: [0, 1],
+      easing: 'easeOutExpo',
+      duration: 1200,
+      delay: (el, i) => 500 + 30 * i
+    });
+  };
+
+  const slideUp = (element) => {
+    anime({
+      targets: element,
+      translateY: [40, 0],
+      translateZ: 0,
+      opacity: [0, 1],
+      easing: 'easeOutExpo',
+      duration: 1200,
+      delay: (el, i) => 500 + 30 * i
+    });
+  };
+
   document.addEventListener('DOMContentLoaded', (event) => {
     if (document.querySelector('.animation-module--cube')) {
       cubeAnimation(document.querySelector('.animation-module--cube'));
     }
+
+    if ('IntersectionObserver' in window && document.querySelectorAll('.project-module .module__kicker')) {
+      const projectModuleKickerElements = document.querySelectorAll('.project-module .module__kicker');
+
+      const projectModuleKickerObserver = new IntersectionObserver((elements) => {
+        elements.forEach((element) => {
+          if (element.intersectionRatio <= 0) return;
+
+          projectModuleKickerObserver.unobserve(element.target);
+          movingLettersEffect12(element.target);
+        });
+      });
+
+      projectModuleKickerElements.forEach((element) => {
+        element.innerHTML = element.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+        const letters = element.querySelectorAll('.letter');
+        letters.forEach((item) => item.style.opacity = 0);
+        projectModuleKickerObserver.observe(element);
+      });
+    }
+
+    if ('IntersectionObserver' in window && document.querySelectorAll('.project-module .module__heading')) {
+      const projectModuleHeadingElements = document.querySelectorAll('.project-module .module__heading');
+
+      const projectModuleHeadingObserver = new IntersectionObserver((elements) => {
+        elements.forEach((element) => {
+          if (element.intersectionRatio <= 0) return;
+
+          projectModuleHeadingObserver.unobserve(element.target);
+          slideUp(element.target);
+        });
+      });
+
+      projectModuleHeadingElements.forEach((element) => {
+        element.style.opacity = 0;
+        projectModuleHeadingObserver.observe(element);
+      });
+    }
+
+    if ('IntersectionObserver' in window && document.querySelectorAll('.project-module__services')) {
+      const projectModuleServicesElements = document.querySelectorAll('.project-module__services');
+
+      const projectModuleServicesObserver = new IntersectionObserver((elements) => {
+        elements.forEach((element) => {
+          if (element.intersectionRatio <= 0) return;
+
+          projectModuleServicesObserver.unobserve(element.target);
+          slideUp(element.target.querySelectorAll('li'));
+        });
+      });
+
+      projectModuleServicesElements.forEach((element) => {
+        const items = element.querySelectorAll('li');
+        items.forEach((item) => item.style.opacity = 0);
+        projectModuleServicesObserver.observe(element);
+      });
+    }
   });
-})();
+})(anime);
